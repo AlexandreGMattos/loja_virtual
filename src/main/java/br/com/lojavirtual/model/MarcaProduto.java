2f43fa8a619bc2776b2d4b3ebd82f60f6094822d
AlexandreGMattos <alexandregmattos@gmail.com>
2024-12-27 07:19:53 -0300

    commit - criando a classe categoriaProduto

--- src/main/java/br/com/lojavirtual/model/MarcaProduto.java
+++ src/main/java/br/com/lojavirtual/model/MarcaProduto.java
@@ -1,7 +1,8 @@
-package br.com.model;
+package br.com.lojavirtual.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,6 +21,7 @@ public class MarcaProduto implements Serializable{
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
 	private Long id;
 	
+	@Column(nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
@@ -36,6 +38,31 @@ public class MarcaProduto implements Serializable{
 
 	public void setNomeDesc(String nomeDesc) {
 		this.nomeDesc = nomeDesc;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MarcaProduto other = (MarcaProduto) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
