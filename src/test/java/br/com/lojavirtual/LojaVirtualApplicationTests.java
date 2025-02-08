9621f82cce360e90ac069e9d2be49b5bc506b433
AlexandreGMattos <alexandregmattos@gmail.com>
2025-02-08 08:39:53 -0300

    testando Junit

--- src/test/java/br/com/lojavirtual/LojaVirtualApplicationTests.java
+++ src/test/java/br/com/lojavirtual/LojaVirtualApplicationTests.java
@@ -3,7 +3,7 @@ package br.com.lojavirtual;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ActiveProfiles;   
 
 import br.com.lojavirtual.model.Acesso;
 import br.com.lojavirtual.repository.AcessoRepository;
