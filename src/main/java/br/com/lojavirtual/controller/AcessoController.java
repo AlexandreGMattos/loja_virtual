4f0fb5e92cdc714244d73ab106eddea1533f506a
AlexandreGMattos <alexandregmattos@gmail.com>
2025-02-22 07:13:17 -0300

    AULA - Criando End-Point e teste com Junit

--- src/main/java/br/com/lojavirtual/controller/AcessoController.java
+++ src/main/java/br/com/lojavirtual/controller/AcessoController.java
@@ -1,20 +1,43 @@
 package br.com.lojavirtual.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.lojavirtual.model.Acesso;
+import br.com.lojavirtual.repository.AcessoRepository;
 import br.com.lojavirtual.service.AcessoService;
 
-@Controller
+@Controller /*indica que a classe é responsável por lidar com as requisições HTTP e retornar as respostas, geralmente em forma de uma visão (view) como uma página HTML, JSON, ou XML*/
+@RestController
 public class AcessoController {
 	
 	@Autowired
 	private AcessoService acessoService;
 	
-	@PostMapping("/salvarAcesso")
-	public Acesso salvarAcesso(Acesso acesso) {
-		return acessoService.save(acesso);
+	@Autowired
+	private AcessoRepository acessoRepository;
+	
+	@ResponseBody /*pode dar um retorno da API*/
+	@PostMapping(value = "**/salvarAcesso") /*mapeando a url para receber um JSON*/
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) { /*@RequestBody recebe o JSON e converte para objeto, se nao colocar fica tudo nulo*/
+		
+		Acesso acessoSalvo = acessoService.save(acesso);
+		
+		return new ResponseEntity<Acesso>(acessoSalvo, HttpStatus.OK);
+	}
+	
+	@ResponseBody 
+	@PostMapping(value = "**/deleteAcesso") 
+	public ResponseEntity<?> deleteAcesso(@RequestBody Acesso acesso) { 
+		
+		acessoRepository.delete(acesso);
+		
+		return new ResponseEntity("Acesso Removido.", HttpStatus.OK);
 	}
 }
--- src/test/java/br/com/lojavirtual/LojaVirtualApplicationTests.java
+++ src/test/java/br/com/lojavirtual/LojaVirtualApplicationTests.java
@@ -1,5 +1,7 @@
 package br.com.lojavirtual;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,11 +17,11 @@ import junit.framework.TestCase;
 @ActiveProfiles("test")
 public class LojaVirtualApplicationTests extends TestCase{
 
-	//@Autowired
-	//private AcessoService acessoSevice; 
+	@Autowired
+	private AcessoService acessoSevice; 
 	
-	//@Autowired
-	//private AcessoRepository acessoRepository;
+	@Autowired
+	private AcessoRepository acessoRepository;
 	
 	@Autowired
 	private AcessoController acessoController;
@@ -29,9 +31,46 @@ public class LojaVirtualApplicationTests extends TestCase{
 		Acesso acesso = new Acesso();
 		
 		acesso.setDescricao("ROLE_TEST");
-		
-		acessoController.salvarAcesso(acesso);
 		//acessoRepository.save(acesso);
+		
+		assertNull(acesso.getId()); // Certifica que o objeto ainda não foi salvo
+		
+		/*Gravou no banco de dados*/
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		assertEquals(true, acesso.getId() > 0);
+		
+		/*Validar dados salvo de forma correta*/
+		assertEquals("ROLE_TEST", acesso.getDescricao());  
+		
+		/*Teste de carregamento*/
+		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
+		
+		assertEquals(acesso.getId(), acesso2.getId());
+		
+		/*Teste de delete*/
+		acessoRepository.deleteById(acesso2.getId());
+		acessoRepository.flush(); // roda esse SQl de delete no banco de dados
+		
+		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
+		
+		assertEquals(true, acesso3 == null);
+		
+		
+		/*Teste de Query*/
+		
+		acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ALUNO");
+		
+		acesso = acessoController.salvarAcesso(acesso).getBody();
+		
+		List<Acesso> acessos = acessoRepository.buscarAcessoDesc("ALUNO".trim().toUpperCase());
+		
+		assertEquals(1, acessos.size());
+		
+		acessoRepository.deleteById(acesso.getId());
+		
 	}
 
 }
